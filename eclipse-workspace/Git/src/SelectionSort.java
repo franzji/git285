6cb654f99d84c1618957ee52ced60e349297bfc8
Woodwing <woodwing@Woodwings-MacBook-Air-3.local>
2018-08-21 15:07:42 -0400

    commit 2

--- eclipse-workspace/Git/src/SelectionSort.java
+++ eclipse-workspace/Git/src/SelectionSort.java
@@ -3,27 +3,21 @@ public class SelectionSort {
 
 	/** Creates a new instance of SelectionSort */
     public SelectionSort() {
+    	
     }
     
-    /* A simple SelectionSort algorithm
-     * pre-condition: 
-     * post-condition:
-     * inputs:
-     * outputs:
-     * special conditions:
-     */    
-    
     public int[] basicSelectionSort(int[] x) {
-        for (int i = 0; i < x.length; ++i) {
-            for (int j= i+1; j < x.length; ++j) {
-                if (x[i] > x[j]) {
-                    temp = x[i];
-                    x[i] = x[j];
-                    temp = x[j];
+        for (int i = 0; i < x.length-1; i++) {
+        	int min = i;
+            for (int j= i+1; j < x.length; j++) {
+                if (x[j] < x[min]) {
+                	min = j;
                 }
             } // end of inner for loop
+            temp = x[min];
+            x[min] = x[i];
+            x[i] = temp;
         } // end of outer for loop
         return x;
     } // end of basicSelectionSort method
-
 }
--- eclipse-workspace/Git/src/testSelectionSort.java
+++ eclipse-workspace/Git/src/testSelectionSort.java
@@ -1,5 +1,4 @@
 import static org.junit.Assert.*;
-import org.junit.Assert;
 import org.junit.Test;
 
 public class testSelectionSort {
@@ -7,13 +6,10 @@ public class testSelectionSort {
 	public void test() {
 		testPositive();
 		testNegative();
-		testZeros();
+		testMixed();
 		testDuplicates();
 	}
-
-    public testSelectionSort() {
-    }
-    
+	
     public void testPositive(){
         int[] arr = new int[5];
         arr[0] = 8;
@@ -29,16 +25,75 @@ public class testSelectionSort {
         Sortedarr[3] = 9;
         Sortedarr[4] = 10;
         
-        /** add tests to check for this unit test **/
+        SelectionSort temp1 = new SelectionSort();
+        arr = temp1.basicSelectionSort(arr);
+        for(int i = 0; i < 5;i++) {
+            assertTrue("not same", Sortedarr[i] == arr[i]);
+        }
     }
     
     public void testNegative(){
+        int[] arr = new int[5];
+        arr[0] = -8;
+        arr[1] = -9;
+        arr[2] = -7;
+        arr[3] = -10;
+        arr[4] = -2;
         
+        int[] Sortedarr = new int[5];
+        Sortedarr[0] = -10;
+        Sortedarr[1] = -9;
+        Sortedarr[2] = -8;
+        Sortedarr[3] = -7;
+        Sortedarr[4] = -2;
         /** Test data contains negative values only **/
+        
+        SelectionSort temp1 = new SelectionSort();
+        arr = temp1.basicSelectionSort(arr);
+        for(int i = 0; i < 5;i++) {
+            assertTrue("not same", Sortedarr[i] == arr[i]);
+        }
     }
-    public void testZeros(){
-    	
-    }
+    public void testMixed(){
+        int[] arr = new int[5];
+        arr[0] = -1;
+        arr[1] = 4;
+        arr[2] = 2;
+        arr[3] = -5;
+        arr[4] = 5;
+        
+        int[] Sortedarr = new int[5];
+        Sortedarr[0] = -5;
+        Sortedarr[1] = -1;
+        Sortedarr[2] = 2;
+        Sortedarr[3] = 4;
+        Sortedarr[4] = 5;
+        
+        SelectionSort temp1 = new SelectionSort();
+        arr = temp1.basicSelectionSort(arr);
+        for(int i = 0; i < 5;i++) {
+            assertTrue("not same", Sortedarr[i] == arr[i]);
+        }
     }
     public void testDuplicates(){
+        int[] arr = new int[5];
+        arr[0] = 1;
+        arr[1] = -2;
+        arr[2] = 1;
+        arr[3] = -2;
+        arr[4] = 1;
+        
+        int[] Sortedarr = new int[5];
+        Sortedarr[0] = -2;
+        Sortedarr[1] = -2;
+        Sortedarr[2] = 1;
+        Sortedarr[3] = 1;
+        Sortedarr[4] = 1;
+        
+        SelectionSort temp1 = new SelectionSort();
+        arr = temp1.basicSelectionSort(arr);
+        for(int i = 0; i < 5;i++) {
+            assertTrue("not same", Sortedarr[i] == arr[i]);
+        }
     }
+}
